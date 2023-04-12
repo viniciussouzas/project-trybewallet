1caf17ff5b0fb0b4331a62c47852da3a14a499ca
Vinícius Souza <tradutorvinicius@gmail.com>
2023-04-12 10:42:50 -0300

    Resolve requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addEmail } from '../redux/actions/index';
 
-class Login extends React.Component {
+const MIN_PASSWORD_LENGTH = 6;
+const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState(({
+      [name]: value,
+    }));
+  };
+
+  handleClickLogin = (e) => {
+    e.preventDefault();
+
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+
+    dispatch(addEmail(email));
+
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    const isValid = validEmail.test(email) && password.length >= MIN_PASSWORD_LENGTH;
+
+    return (
+      <div>
+        <form>
+          <label htmlFor="email-input">
+            <input
+              type="text"
+              name="email"
+              value={ email }
+              onChange={ this.onInputChange }
+              data-testid="email-input"
+              placeholder="Insira seu e-mail"
+            />
+          </label>
+          <label htmlFor="password-input">
+            <input
+              type="password"
+              name="password"
+              value={ password }
+              onChange={ this.onInputChange }
+              data-testid="password-input"
+              placeholder="Insira sua senha"
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ !isValid }
+            onClick={ this.handleClickLogin }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions
+export const ADD_EMAIL = 'ADD_EMAIL';
+
+export const addEmail = (email) => ({
+  type: ADD_EMAIL,
+  payload: email,
+});
